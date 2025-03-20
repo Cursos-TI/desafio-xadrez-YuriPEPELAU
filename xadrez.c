8906aeec60b9d045d7ddf98feae60c5598790e96
YuriPEPELAU <yurialvesherdy@gmail.com>
2025-03-20 18:24:04 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,168 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
-
-    return 0;
+int main(){
+// Declaração de variavéis
+int t,b=0,r=0,opcao,opcao2;
+// Menu de escolha do usuário da peça que deseja mover
+printf("1. Torre\n");
+printf("2. Bispo\n");
+printf("3. Rainha\n");
+printf("Escolha a peca que deseja mover: ");
+scanf("%i",&opcao);
+switch(opcao){
+       case 1:
+       // Menu de escolha do usuário da direção que deseja mover a 'Torre'
+       printf("1. Cima\n");
+       printf("2. Esquerda\n");
+       printf("3. Direita\n");
+       printf("4. Baixo\n");
+       printf("Escolha a direcao que deseja mover a peca: ");
+       scanf("%i",&opcao2);
+       switch(opcao2){
+              case 1:
+                     // Estrutura de repetição 'for'
+                     for(t=0;t<5;t++){
+                            printf("Cima\n");
+                     }
+              break;
+              case 2:
+                     // Estrutura de repetição 'for'
+                     for(t=0;t<5;t++){
+                            printf("Esquerda\n");
+                     }
+              break;
+              case 3:
+                     // Estrutura de repetição 'for'
+                     for(t=0;t<5;t++){
+                            printf("Direita\n");
+                     }
+              break;
+              case 4:
+                     // Estrutura de repetição 'for'
+                     for(t=0;t<5;t++){
+                            printf("Baixo\n");
+                     }
+              break;
+              default: printf("Opcao Invalida\n");
+              break;
+       }
+       break;
+       case 2:
+       // Menu de escolha do usuário da direção que deseja mover o 'Bispo'
+       printf("1. Cima-direita\n");
+       printf("2. Cima-esquerda\n");
+       printf("3. Baixo-direita\n");
+       printf("4. Baixo-esquerda\n");
+       printf("Escolha a direcao que deseja mover a peca: ");
+       scanf("%i",&opcao2);
+       switch(opcao2){
+              case 1:
+                     // Estrutura de repetição 'while'
+                     while(b<5){
+                            printf("Cima-direita\n");
+                            b++;
+                     }
+              break;
+              case 2:
+                     // Estrutura de repetição 'while'
+                     while(b<5){
+                            printf("Cima-esquerda\n");
+                            b++;
+                     }
+              break;
+              case 3:
+                     // Estrutura de repetição 'while'
+                     while(b<5){
+                            printf("Baixo-direita\n");
+                            b++;
+                     }
+              break;
+              case 4:
+                     // Estrutura de repetição 'while'
+                     while(b<5){
+                            printf("Baixo-esquerda\n");
+                            b++;
+                     }
+              break;
+              default: printf("Opcao Invalida\n");
+              break;
+       }
+       break;
+       case 3:
+       // Menu de escolha do usuário da direção que deseja mover a 'Rainha'
+       printf("1. Cima\n");
+       printf("2. Esquerda\n");
+       printf("3. Direita\n");
+       printf("4. Baixo\n");
+       printf("5. Cima-direita\n");
+       printf("6. Cima-esquerda\n");
+       printf("7. Baixo-direita\n");
+       printf("8. Baixo-esquerda\n");
+       printf("Escolha a direcao que deseja mover a peca: ");
+       scanf("%i",&opcao2);
+       switch(opcao2){
+              case 1:
+                     // Estrutura de repetição 'do-while'
+                     do{
+                            printf("Cima\n");
+                            r++;
+                     }while(r<8);
+              break;
+              case 2:
+                     // Estrutura de repetição 'do-while'
+                     do{
+                            printf("Esquerda\n");
+                            r++;
+                     }while(r<8);
+              break;
+              case 3:
+                     // Estrutura de repetição 'do-while'
+                     do{
+                            printf("Direita\n");
+                            r++;
+                     }while(r<8);
+              break;
+              case 4:
+                     // Estrutura de repetição 'do-while'
+                     do{
+                            printf("Baixo\n");
+                            r++;
+                     }while(r<8);
+              break;
+              case 5:
+                     // Estrutura de repetição 'do-while'
+                     do{
+                            printf("Cima-direita\n");
+                            r++;
+                     }while(r<8);
+              break;
+              case 6:
+                     // Estrutura de repetição 'do-while'
+                     do{
+                            printf("Cima-esquerda\n");
+                            r++;
+                     }while(r<8);
+              break;
+              case 7:
+                     // Estrutura de repetição 'do-while'
+                     do{
+                            printf("Baixo-direita\n");
+                            r++;
+                     }while(r<8);
+              break;
+              case 8:
+                     // Estrutura de repetição 'do-while'
+                     do{
+                            printf("Baixo-esquerda\n");
+                            r++;
+                     }while(r<8);
+              break;
+              default: printf("Opcao Invalida\n");
+              break;
+       }
+       break;
+       default: printf("Opcao Invalida\n");
+       break;
+      }
+       return 0;
 }
